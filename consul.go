d7224085e0013cd9b3ed23b21b508e3123e716b4
karziolynas <125891596+karziolynas@users.noreply.github.com>
2025-04-26 13:31:05 +0300

    updated time

--- consul.go
+++ consul.go
@@ -180,7 +180,7 @@ func (s *Service) WatchHealthChecks(consulAddress, handlerURL string) {
 						"CheckName":     check.CheckID,
 						"CheckType":     check.Type,
 						"CheckStatus":   check.Status,
-						"FailureTime":   time.Now().GoString(),
+						"FailureTime":   time.Now().Format("2006-01-02 15:04:05"),
 						"FailureOutput": check.Output,
 					})
 					responseBody := bytes.NewBuffer(postBody)
